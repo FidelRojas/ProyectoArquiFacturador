17282a00715bd1a7010dd5af47cd9ff23f11a484
FidelRojas <45080814+FidelRojas@users.noreply.github.com>
2020-06-18 11:29:22 -0400

    fixet null telefono

--- Facturador/src/app/home/home.component.ts
+++ Facturador/src/app/home/home.component.ts
@@ -12,31 +12,34 @@ export class HomeComponent implements OnInit {
   numero = "";
   nombre = "";
   nit = "";
-
-  mes = "Enero"
+  error="";
+  mes = "1"
   constructor(private facturacionService: FacturacionService, private router: Router) { }
   form: FormGroup;
   ngOnInit(): void {
 
   }
   onSubmit() {
-    console.log(this.numero, this.mes);
-
-    this.facturacionService.getFactura(this.numero, this.mes).subscribe(res => {
-      console.log(res);
+    if(this.numero&&this.mes){
+      this.facturacionService.getFactura(this.numero, this.mes).subscribe(res => {
+        console.log(res);
+        
+        this.facturacionService.registroDeFacturas = res["lista"];
+        this.facturacionService.costo = res["suma"];
+        console.log(res["suma"]);
+        this.facturacionService.nombre = this.nombre;
+        this.facturacionService.nit = this.nit;
+        this.facturacionService.numero = this.numero;
+        this.facturacionService.mes = this.mes;
+        
+        
+        this.router.navigate(['/factura']);
+      }, err => this.router.navigate(['/factura']))
       
-      this.facturacionService.registroDeFacturas = res["lista"];
-      this.facturacionService.costo = res["suma"];
-      console.log(res["suma"]);
-      this.facturacionService.nombre = this.nombre;
-      this.facturacionService.nit = this.nit;
-      this.facturacionService.numero = this.numero;
-      this.facturacionService.mes = this.mes;
-
-
-      this.router.navigate(['/factura']);
-    }, err => this.router.navigate(['/factura']))
-
+    }
+    else{
+      this.error="Introdusca un numero valido!";
+    }
   }
 
 }
